c4be584596fdc4f637e9110a92e392fb6f5118e6
ericpkatz <ericpkatz@gmail.com>
2023-09-21 12:15:28 -0400

    added react app

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const pg = require('pg');
 const client = new pg.Client('postgres://localhost/fullstack_template_db');
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const homePage = path.join(__dirname, 'index.html');
+
+app.get('/', (req, res)=> res.sendFile(homePage));
+
+const reactApp = path.join(__dirname, 'dist/main.js');
+
+app.get('/dist/main.js', (req, res)=> res.sendFile(reactApp));
 
 const init = async()=> {
   await client.connect();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(React.createElement('h1', null, 'Hello world'));
