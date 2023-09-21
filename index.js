898ac807b3fa26340594be75f2fdbc993e6ee268
ericpkatz <ericpkatz@gmail.com>
2023-09-21 12:44:15 -0400

    support for jsx

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const app = express();
 const path = require('path');
 
 const homePage = path.join(__dirname, 'index.html');
-
 app.get('/', (req, res)=> res.sendFile(homePage));
 
 const reactApp = path.join(__dirname, 'dist/main.js');
-
 app.get('/dist/main.js', (req, res)=> res.sendFile(reactApp));
+
+const reactSourceMap = path.join(__dirname, 'dist/main.js.map');
+app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
+
+const styleSheet = path.join(__dirname, 'styles.css');
+app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
 const init = async()=> {
   await client.connect();
@@ -18,6 +22,7 @@ const init = async()=> {
   const SQL = `
     SQL SETUP AND SEED
   `;
+  console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;
   app.listen(port, ()=> {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
+const App = ()=> {
+  return (
+    <h1>FullStack Template</h1>
+  );
+};
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-
-root.render(React.createElement('h1', null, 'Hello world'));
+root.render(<App />);
